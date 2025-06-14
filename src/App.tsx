4033676d537d458d20605f7a53b29aa1d2bd7a72
adrianodisam <adrianodisam@gmail.com>
2025-06-14 20:27:48 -0300

    porjeto etapa 2.0

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,9 @@
 import './App.css'
-import Fatorar from './componentes/fatorarar/Fatorar'
 import ContainerPrincipal from './componentes/containerPrincipal/ContainerPrincipal'
-
 function App() {
- 
   return (
     <>
       <ContainerPrincipal/>
-      {/* <Fatorar/> */}
     </>
   )
 }
