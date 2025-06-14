a0cd417c8491a0eb7d8b0112532fd0c998c5b214
adrianodisam <adrianodisam@gmail.com>
2025-06-14 20:30:50 -0300

    porjeto etapa 2.0

--- src/componentes/containerPrincipal/ContainerPrincipal.tsx
+++ src/componentes/containerPrincipal/ContainerPrincipal.tsx
@@ -1,7 +1,5 @@
 import styled, { createGlobalStyle } from 'styled-components';
 import Cabecalho from '../cabecalho/Cabecalho';
-import Centro from '../centro/Centro';
-import Final from '../final/Final';
 function ContainerPrincipal() {
   const GlobalStyle = createGlobalStyle`
     *, *::before, *::after {
@@ -19,8 +17,6 @@ function ContainerPrincipal() {
   const Principal = styled.div`
     display: flex;
     justify-content: center;
-    /* align-items: center; */
-    /* flex-direction: column; */
     width: 100%;
     height: 100vh;
     background-color: #f7f2f6;
@@ -30,23 +26,12 @@ function ContainerPrincipal() {
     margin: 0;
     padding: 0;
   `;
-  // const Principal = styled.div`
-  //   display: flex;
-  //   width: 100%;
-  //   max-width: 900px;
-  //   height: 400px;
-  //   background-color: #f7f2f6;
-  //   font-family: 'Arial', sans-serif;
-  //   box-shadow: 0 4px 12px rgba(0, 0, 0, 0.1);
-  // `;
 
   return (
     <>
       <GlobalStyle />
       <Principal> 
         <Cabecalho/>
-        {/* <Centro/> */}
-        {/* <Final/> */}
       </Principal>
     </>
   )
